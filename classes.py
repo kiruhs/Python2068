bab64df532b3e9234383dc2ed47f2f43d5778e28
kiruhs <87802917+kiruhs@users.noreply.github.com>
2024-10-16 17:18:51 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -80,30 +80,3 @@ class Rectangle:
 # # print(pt2.__dict__)
 # # print(Point.__dict__)
 # print(pt1.MIN_COORD)
-
-class FibonacciIterator:
-    def __init__(self, stop=10):
-        self._stop = stop
-        self._index = 0
-        self._current = 0
-        self._next = 1
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self._index < self._stop:
-            self._index += 1
-            fib_number = self._current
-            self._current, self._next = (
-                self._next,
-                self._current + self._next,
-            )
-            return fib_number
-        else:
-            raise StopIteration
-fi = FibonacciIterator()
-for fib_number in fi:
-    print(fib_number)
-
-print(next(fi))
